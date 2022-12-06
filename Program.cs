a4b9960dff524f29d3de8d8258a60120203cbebb
fpmovec <10.maks.10K@gmail.com>
2022-12-06 14:08:10 +0300

    Added first task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] cars = { "Nissan", "Aston Martin", "Chevrolet", "Alfa Romeo", "Chrysler", "Dodge", "BMW",
+       "Ferrari", "Audi", "Bentley", "Ford", "Lexus", "Mercedes", "Toyota", "Volvo", "Subaru", "Жигули :)"};
+
+var item = from p in cars
+           where p.ToUpper().StartsWith("F")
+           select p;
+var itemOne = cars.Where(p => p.ToUpper().StartsWith("F")).Select(p => p);
+foreach (var it in itemOne)
+{
+    Console.WriteLine(it);
+}
